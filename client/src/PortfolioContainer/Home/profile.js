c1689d0f99b81b42b880ceddf0e100e13ece135e
Muntaas Hassan <Muntaas.hassan@gmail.com>
2022-02-23 18:13:34 -0500

    new folders added

---
+++ client/src/PortfolioContainer/Home/profile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href='#'>
+                            <i className='fa fa-facebok-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-google-plus-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-instagram'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-youtube-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-twitter-square'></i>
+                        </a>
+                        
+                    </div>
+                </div>
+        </div>
+'}'
